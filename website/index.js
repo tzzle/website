eb64696f6f141712cdf8a08637d1dac77a450a54
tzzle <130739462+tzzle@users.noreply.github.com>
2023-05-15 18:48:18 +0300

    Update index.js

--- website/index.js
+++ website/index.js
@@ -9,3 +9,41 @@ function disappear() {
     document.getElementById('remove').style.display = 'flex';
     document.getElementById('content').style.display = 'block';
 }
+
+function courseAction() {
+    document.getElementById('courseActionButton').style.display = 'none'
+    document.getElementById('courseActionButtonDown').style.display = 'flex';
+    document.getElementById('course-vid1').style.display = 'flex';
+    document.getElementById('course-vid2').style.display = 'flex';
+}
+
+function courseActionClose() {
+    document.getElementById('courseActionButtonDown').style.display ='none';
+    document.getElementById('courseActionButton').style.display = 'flex';
+    document.getElementById('course-vid1').style.display = 'none';
+    document.getElementById('course-vid2').style.display = 'none';
+}
+
+function down() {
+    document.getElementById('pre_f_s_b_more').style.display = 'none';
+    document.getElementById('c_f_i').style.display = 'block';
+    document.getElementById('pref_s_b_less').style.display = 'flex';
+}
+
+function up() {
+    document.getElementById('pre_f_s_b_more').style.display = 'flex';
+    document.getElementById('c_f_i').style.display = 'none';
+    document.getElementById('pref_s_b_less').style.display = 'none';
+}
+
+function downOne() {
+    document.getElementById('d1-buttonOne').style.display = 'none';
+    document.getElementById('c_s_i').style.display = 'block';
+    document.getElementById('pref-f-s-b-lessTwo').style.display = 'flex';
+}
+
+function upOne() {
+    document.getElementById('d1-buttonOne').style.display = 'flex';
+    document.getElementById('c_s_i').style.display = 'none';
+    document.getElementById('pref-f-s-b-lessTwo').style.display = 'none';
+}
